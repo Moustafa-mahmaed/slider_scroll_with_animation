d07943dccf79025972c317fb4d3d37af9adca284
moustafamahamedshamaa <moustafamahamedshamaa@gmail.com>
2020-03-27 16:44:50 +0200

    add scrollview horizonal and gradiant color as background

--- App.js
+++ App.js
@@ -1,12 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 
-import React from 'react';
+import React, { Component } from 'react'
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,101 +7,184 @@ import {
   View,
   Text,
   StatusBar,
+    Animated,
+    Dimensions,
+    Image
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+import Svg ,{Rect ,Stop  ,RadialGradient ,Defs} from "react-native-svg"
+const {width ,height}=Dimensions.get("window");
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+ const product_data=[
+    {
+        id:"1",
+        title:"TMA-2 HD Wireless",
+        subTite:"Wireless" ,
+        description:"The ultimate listening experience. Highly detailed and precise sound representation, wirelessly transmitted ",
+        price:"25$",
+        bg:"#16cdc1",
+        urlimg:"https://6f836c397566f8a68572-e2de800189bc8603e0746245fbc4e3cb.ssl.cf3.rackcdn.com/tma-2-hd-wireless-IigQOPQt-large.png"
 
-export default App;
+    },
+    {
+        id:"2",
+        title:"TMA-2 HD Wireless 2",
+        subTite:"Wireless 2" ,
+        description:"The ultimate listening experience. Highly detailed and precise sound representation, wirelessly transmitted ",
+        price:"20$",
+        bg:"purple",
+        urlimg:"https://6f836c397566f8a68572-e2de800189bc8603e0746245fbc4e3cb.ssl.cf3.rackcdn.com/tma-2-hd-DnUVYeYY-large.png"
+
+    },
+    {
+        id:"3",
+        title:"TMA-2 HD Wireless",
+        subTite:"Wireless" ,
+        description:"The ultimate listening experience. Highly detailed and precise sound representation, wirelessly transmitted ",
+        price:"25$",
+        bg:"red",
+        urlimg:"https://6f836c397566f8a68572-e2de800189bc8603e0746245fbc4e3cb.ssl.cf3.rackcdn.com/tma-2-move-kua6P7RY-large.png"
+
+    }
+  
+]
+
+
+
+
+export default class App extends Component {
+  _scrollX =new Animated.Value(0)
+  renderItem =(item ,i)=>{
+    return(
+      <View key={item.id} style={[styles.container ,styles.item]}>
+      <Image 
+      style={styles.image}
+       source={{uri: item.urlimg}} />
+     <View style={styles.metacontainer}>
+      <Text style={[styles.font ,styles.title]}>{item.title}</Text>
+      <Text style={[styles.font ,styles.subTite]} >{item.subTite}</Text>
+      <Text style={[styles.font ,styles.description]} >{item.description}</Text>
+       <Text style={[styles.font , styles.price]}>{item.price}</Text>
+     </View>
+     {this.renderRadiusGradient(item.bg)}
+      </View>
+    )
+
+  }
+
+
+  renderRadiusGradient =(color) =>{
+    return(
+      <View style={styles.svgContainer}>
+      <Svg height={height} 
+      width={width} 
+      style={styles.bgRadialGradient} >
+      <Defs>
+      <RadialGradient 
+        id="grad"
+       cx="50%"
+      cy="35%"
+      r="60%"
+      gradientUnits="userSpaceOnUse"
+      >
+        <Stop offset="0%" stopColor="#fff" stopOpacity="1" />
+      <Stop offset="100%" stopColor={color} stopOpacity="1" />
+</RadialGradient >
+      </Defs>
+       <Rect
+    x="0"
+    y="0"
+    width={width}
+    height={height}
+    fill={`url(#grad)`} 
+    fillOpacity=".9"
+  />
+      
+      </Svg>
+      </View>
+    )
+  }
+  render() {
+    return (
+      <View>
+       <Animated.ScrollView 
+       pagingEnabled
+       scrollEventThrottle={16}
+       horizontal
+       onScroll={Animated.event(
+         [{nativeEvent:{contentOffset :{x:this._scrollX}}}],
+         {useNativeDriver: true}
+       )}
+       contentContainerStyle={styles.ScrollViewContainer}
+       >
+       {
+         product_data.map((item ,i)=>this.renderItem(item, i)
+         )
+       }
+       </Animated.ScrollView>
+      </View>
+    )
+  }
+}
+
+const styles=StyleSheet.create({
+  ScrollViewContainer:{
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+  ,container:{
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  item:{
+    width,
+    height,
+    alignItems:"center"
+
+  }
+  ,
+  metacontainer:{
+    alignItems:"center",
+    justifyContent:"flex-end",
+    backgroundColor:"transparent",
+    padding:15
+  }
+  ,title:{
+        fontSize:36,
+        fontWeight:"900"
+  },
+  subTite:{
+    fontSize:20,
+        fontWeight:"900"
+
+  },
+  description:{
+    fontSize:14,
+       marginVertical: 15,
+       textAlign:"center"
+
+  },
+  price:{
+      fontSize:42,
+      fontWeight:"400"
+  },
+  font:{
+    color:"#222"
+
+  },
+  image:{
+    height:height*.65,
+    width:width*.85,
+    resizeMode:"contain"
+
+  },
+  svgContainer:{
+position:"absolute",
+top:0,
+left:0,
+zIndex:-1
+  }
+})
